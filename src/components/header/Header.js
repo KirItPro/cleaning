220835365a7dcbe3598114c0693b1e12161cecf5
KirItPro <117309466+KirItPro@users.noreply.github.com>
2024-01-24 14:20:33 +0300

    add to router on header logo and change data price

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,25 @@
+import { Link } from "react-scroll";
 import HEADERLOGO from '../../img/header-logo.png';
 import POINT from '../../img/point.png';
 import MenuNavigation from '../navigation/MenuNavigation';
 import './header.css';
+import { menuData } from "../navigation/menu.data";
 
 export default function Header() {
     return (
       <div className="container">
         <header className="header">
-          <img className='imgLogo' src={HEADERLOGO} alt='img-logo' width='150px'/>
+          <Link 
+                className='imgLogo'
+                spy={true} 
+                to={menuData[0].id}                 
+                smooth={true}                    
+                offset={-70}
+                duration= {800}
+                key={menuData[0].id}>
+            <img src={HEADERLOGO} alt='img-logo' width='150px'/>
+          </Link>
+
           <div className='place-time'>
             <p>Ежедневно с 9:00 до 21:00</p>
             <p><img className='point' src={POINT } alt='point' width="30px"/>г.Краснодар</p>
